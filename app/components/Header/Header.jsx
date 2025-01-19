17974bee1dfc5eb01cc023232131959a33bbea2f
zunairaihsan <59208594+zunairaihsan@users.noreply.github.com>
2025-01-19 20:43:43 +0500

    formatted

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -4,32 +4,32 @@ import AuthContextProvider from '@/lib/contexts/AuthContext'
 import Link from 'next/link';
 
 export default function Header() {
-    return <nav className="flex justify-between items-center px-7 py-3 border-b">
-        <Link href={'/'}>
-            <img className="h-10" src="/logo.png" alt="" />
-        </Link>
-        <ul className="flex gap-6 items-center">
-            <Link href={'/'}>
-                <li className='flex items-center gap-2'>
-                    <Home />
-                    Home
-                </li>
-            </Link>
-            <Link href={'/categories'}>
-                <li className='flex items-center gap-2'>
-                    <List />
-                    Categories
-                </li>
-            </Link>
-            <Link href={'/'}>
-                <li className='flex items-center gap-2'>
-                    <MessageCircle />
-                    Contact Us
-                </li>
-            </Link>
-        </ul>
-        <AuthContextProvider>
-            <LoginButton />
-        </AuthContextProvider>
-    </nav>
+  return <nav className="flex justify-between items-center px-7 py-3 border-b">
+    <Link href={'/'}>
+      <img className="h-10" src="/logo.png" alt="" />
+    </Link>
+    <ul className="flex gap-6 items-center">
+      <Link href={'/'}>
+        <li className='flex items-center gap-2'>
+          <Home />
+          Home
+        </li>
+      </Link>
+      <Link href={'/categories'}>
+        <li className='flex items-center gap-2'>
+          <List />
+          Categories
+        </li>
+      </Link>
+      <Link href={'/'}>
+        <li className='flex items-center gap-2'>
+          <MessageCircle />
+          Contact Us
+        </li>
+      </Link>
+    </ul>
+    <AuthContextProvider>
+      <LoginButton />
+    </AuthContextProvider>
+  </nav>
 }
