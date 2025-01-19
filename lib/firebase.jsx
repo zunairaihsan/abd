17b63c0c6969a621b5818ae022bb020a2c63cfdf
zunairaihsan <59208594+zunairaihsan@users.noreply.github.com>
2025-01-19 19:39:31 +0500

    Initial commit

--- lib/firebase.jsx
+++ lib/firebase.jsx
@@ -4,15 +4,17 @@ import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSANGING_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-};
+    apiKey: "AIzaSyBBALgS_95tais-Ay8EHDxdnL49gcTtEcM",
+    authDomain: "soulspot-16370.firebaseapp.com",
+    projectId: "soulspot-16370",
+    storageBucket: "soulspot-16370.firebasestorage.app",
+    messagingSenderId: "660920684556",
+    appId: "1:660920684556:web:50396a03aa97d4ae1e9e29",
+    measurementId: "G-XQ5NC3R2GQ"
+  };
 
 const app = initializeApp(firebaseConfig);
+
 
 export const db = getFirestore();
 export const auth = getAuth();
